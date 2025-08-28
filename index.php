4822f36b5fe070a6cf694a35d0b73ae339d6e019
devbycpw <christian.peter.dev@gmail.com>
2025-08-28 16:02:00 +0700

    modular

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
-<?php
-    $hello = "hello world";
-?>
+<?php require 'main.php';?>
+  <body>
+    <?php include 'navbar.php';?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?=$hello;?></h1>
-</body>
-</html>
+  <img src="./img/hero.jpg" alt="hero" width="100%">
+
+  </body>
---
+++ services.php
@@ -0,0 +1,6 @@
+<?php require 'main.php';?>
+<body>
+    <?php include 'navbar.php';?>
+
+    services
+</body>
